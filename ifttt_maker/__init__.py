fc594a5ff0945747ad1dd94976c16e1d55c1e30d
sweemeng <sweester@gmail.com>
2015-06-19 21:12:59 +0800

    Make this a package

---
+++ ifttt_maker/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "sweemeng"
+from ifttt_maker import Ifttt
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ifttt_maker',
+      version='0.0.1',
+      description='A simple interface to ifttt maker',
+      url='http://github.com/sweemeng/ifttt_maker',
+      author='sweemeng',
+      author_email='sweester@gmail.com',
+      license='BSD',
+      packages=['ifttt_maker'],
+      zip_safe=False)
